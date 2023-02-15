0d60bd134fee38517c5c1d07b38f13d8207f3558
João Victor <jaovictor.mota@gmail.com>
2023-02-15 09:31:31 -0300

    Testes esperando o valor correto

--- tests/TaxaTest.php
+++ tests/TaxaTest.php
@@ -10,13 +10,14 @@ class TaxaTest extends TestCase {
 
         $taxa = new Taxa();
 
-        $salario = 1500;
+        $salario = '';
         $this->assertEmpty($taxa->calculaTaxa($salario));
 
     }
 
     public function testParaVerSeAVariavelNaoEstaVazia() {
 
+        //Tem que dar erro
         $taxa = new Taxa();
 
         $salario = null;
@@ -31,5 +32,29 @@ class TaxaTest extends TestCase {
         $salario = 1537.45;
         $this->assertIsFloat($taxa->calculaTaxa($salario));
 
+    }
+
+    public function testVerificaValorSetePontoNove() {
+
+        $taxa = new Taxa();
+
+        $salario = 1500.00;
+        $this->assertEquals(7.9, $taxa->calculaTaxa($salario));
+    }
+
+    public function testVerificaValorDezoito() {
+
+        $taxa = new Taxa();
+
+        $salario = 4000.00;
+        $this->assertEquals(18, $taxa->calculaTaxa($salario));
+    }
+
+    public function testVerificaValorVinteECinco() {
+
+        $taxa = new Taxa();
+
+        $salario = 9000.00;
+        $this->assertEquals(25, $taxa->calculaTaxa($salario));
     }
 }
